7dc4d5c0ef4fdb28eafebad15e812bdf05b12dcb
Faizal Dananjaya <62741758+FaizalDananjaya@users.noreply.github.com>
2025-08-25 20:29:24 +0700

    Update migrate.ts

--- lib/db/migrate.ts
+++ lib/db/migrate.ts
@@ -1,15 +1,10 @@
-import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import postgres from 'postgres';
 
-config({
-  path: '.env.local',
-});
-
 const runMigrate = async () => {
   if (!process.env.POSTGRES_URL) {
-    throw new Error('POSTGRES_URL is not defined');
+    throw new Error('❌ POSTGRES_URL is not defined in Environment Variables');
   }
 
   const connection = postgres(process.env.POSTGRES_URL, { max: 1 });
