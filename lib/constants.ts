cd7d23440dcd006bf4c2776b9355fe4be4f9027e
Faizal Dananjaya <62741758+FaizalDananjaya@users.noreply.github.com>
2025-08-26 14:09:12 +0700

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,13 +1,17 @@
+// lib/constants.ts
 import { generateDummyPassword } from './db/utils.node';
 
+// Environment flags
 export const isProductionEnvironment = process.env.NODE_ENV === 'production';
 export const isDevelopmentEnvironment = process.env.NODE_ENV === 'development';
 export const isTestEnvironment = Boolean(
   process.env.PLAYWRIGHT_TEST_BASE_URL ||
     process.env.PLAYWRIGHT ||
-    process.env.CI_PLAYWRIGHT,
+    process.env.CI_PLAYWRIGHT
 );
 
+// Regex untuk mendeteksi user guest
 export const guestRegex = /^guest-\d+$/;
 
+// Dummy password untuk user guest
 export const DUMMY_PASSWORD = generateDummyPassword();
